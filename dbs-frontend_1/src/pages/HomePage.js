1cd949c939b73788f7be7e5092faa20ad869cfbb
Alwyn-Ong <alwyn.ong.2018@sis.smu.edu.sg>
2021-09-30 16:03:17 +0800

    Fixed fetching loop

--- dbs-frontend_1/src/pages/HomePage.js
+++ dbs-frontend_1/src/pages/HomePage.js
@@ -1,37 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { PageWrapper } from '../layout';
+import React, { useState, useEffect } from "react";
+import { PageWrapper } from "../layout";
 // import ImageCarousel from "../components/ImageCarousel"
 // import { Container, Row, Col } from 'react-bootstrap'
-import Item from '../components/item'
-import {
-  CardContainer
-} from "./styled-components";
-import products from '../data/products.json'
-
+import Item from "../components/item";
+import { CardContainer } from "./styled-components";
+import products from "../data/products.json";
 
 export const HomePage = () => {
-  const [productList, setProductList] = useState(products)
+  const [productList, setProductList] = useState(products);
+  const [isFetch, setIsFetch] = useState(true);
   // const productList = products;
-  
-  fetch("http://localhost:5000/products")
-  .then(response => response.json())
-  .then(result => setProductList(result))
-  .catch(error => console.log('error', error));
+
+  if (isFetch) {
+    fetch("http://localhost:5000/products")
+      .then((response) => response.json())
+      .then((result) => {
+        setProductList(result);
+        setIsFetch(false);
+      })
+      .catch((error) => console.log("error", error));
+  }
 
   useEffect(() => {
     onInit();
-  }, [])
+  }, []);
 
   const onInit = async () => {
-    console.log('mounted');
-  }
+    console.log("mounted");
+  };
 
   return (
     <PageWrapper>
       <CardContainer>
         {productList.map((s) => (
-          <Item item={s} >{s}</Item>
-          ))}
+          <Item item={s}>{s}</Item>
+        ))}
       </CardContainer>
     </PageWrapper>
   );
