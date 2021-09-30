f6308baa4a349888da9bb0d4a2c176c9d5af7dba
JohnTYH <mapler098@gmail.com>
2021-09-30 14:01:43 +0800

    remove material ui
    remove material ui

--- dbs-frontend_1/src/components/item.js
+++ dbs-frontend_1/src/components/item.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
 import {
     CardWrapper,
     CardHeader,
